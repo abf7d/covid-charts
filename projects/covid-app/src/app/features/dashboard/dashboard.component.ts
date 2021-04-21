a5d415e0a52aca26ac516c4b1abd39972177c273
Aaron <afriedman111@gmail.com>
2021-04-21 18:23:41 -0400

    fix: only zoom one country

--- projects/covid-app/src/app/features/dashboard/dashboard.component.ts
+++ projects/covid-app/src/app/features/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ import {
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -25,7 +26,19 @@ export class DashboardComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.createChart();
+    this.http
+    .get(
+      '../../../assets/data/countries.json' /*https://gist.githubusercontent.com/GordyD/49654901b07cb764c34f/raw/27eff6687f677c984a11f25977adaa4b9332a2a9/countries-and-states.json'*/ /*('https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json'*/
+    )
+    .subscribe( world =>
+      {
+        const countries = topojson.feature(
+          world,
+          (world as any).objects.countries
+        ).features;
+        let svg = d3.select(this.chart.nativeElement).append('svg');
+    this.createChart(countries, svg);
+      });
   }
 
   formatLabel(value: number) {
@@ -35,11 +48,11 @@ export class DashboardComponent implements OnInit {
     return value;
   }
   selected: any;
-  createChart() {
+  createChart(countries, svg) {
     const width = 1000;
     const height = 700;
-
-    let svg = d3.select(this.chart.nativeElement).append('svg');
+    svg.select('*').remove();
+    
 
     var projection = d3
       .geoMercator()
@@ -51,22 +64,22 @@ export class DashboardComponent implements OnInit {
     // TODO: Get states also and show on zoom:
     // TODO:  http://bl.ocks.org/MaciejKus/61e9ff1591355b00c1c1caf31e76a668
     // TODO: above data comes from github: https://github.com/owid/covid-19-data/tree/master/public/data/
-    this.http
-      .get(
-        '../../../assets/data/countries.json' /*https://gist.githubusercontent.com/GordyD/49654901b07cb764c34f/raw/27eff6687f677c984a11f25977adaa4b9332a2a9/countries-and-states.json'*/ /*('https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json'*/
-      )
-      .subscribe((world) => {
-        console.log(world);
+    // this.http
+    //   .get(
+    //     '../../../assets/data/countries.json' /*https://gist.githubusercontent.com/GordyD/49654901b07cb764c34f/raw/27eff6687f677c984a11f25977adaa4b9332a2a9/countries-and-states.json'*/ /*('https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json'*/
+    //   )
+    //   .subscribe((world) => {
+    //     console.log(world);
         let svgG = svg.attr('width', width).attr('height', height).append('g');
 
         this.http
           .get('../../../assets/data/owid-covid-data.json')
           .subscribe(console.log);
 
-        const countries = topojson.feature(
-          world,
-          (world as any).objects.countries
-        ).features;
+        // const countries = topojson.feature(
+        //   world,
+        //   (world as any).objects.countries
+        // ).features;
         svgG
           .selectAll('path')
           .data(countries)
@@ -78,63 +91,84 @@ export class DashboardComponent implements OnInit {
             const node = d3.select(event.currentTarget);
             if (this.selected === d) {
               this.selected = null;
-              node.attr('transform', `translate(0, 0) scale(1)`);
+              d.selected = false;
+              
+              // node.attr('transform', `translate(0, 0) scale(1)`);
             } else {
+              countries.forEach(c => c.selected = false)
               this.selected = d;
+              d.selected = !d.selected;
               // node.attr('transform', 'scale(1.5)')
               /* transform="translate(-33.925962254490855,-144.64600554243407) scale(1.5932801925711724)"*/
               console.log(d);
               console.log(event);
               console.log(d.properties.name);
-
+             
               // https://stackoverflow.com/questions/25310390/how-does-path-bounds-work
-              var bounds = path.bounds(d),
-                dx = bounds[1][0] - bounds[0][0],
-                dy = bounds[1][1] - bounds[0][1],
-                x = (bounds[0][0] + bounds[1][0]) / 2,
-                y = (bounds[0][1] + bounds[1][1]) / 2,
-                scale = 0.9 / Math.max(dx / width, dy / height),
-                translate = [width / 2 - scale * x, height / 2 - scale * y];
+              // var bounds = path.bounds(d),
+              //   dx = bounds[1][0] - bounds[0][0],
+              //   dy = bounds[1][1] - bounds[0][1],
+              //   x = (bounds[0][0] + bounds[1][0]) / 2,
+              //   y = (bounds[0][1] + bounds[1][1]) / 2,
+              //   scale = 0.9 / Math.max(dx / width, dy / height),
+              //   translate = [width / 2 - scale * x, height / 2 - scale * y];
 
-              node.attr(
-                'transform',
-                `translate(${translate[0]}, ${translate[1]}) scale(${scale})`
-              );
+              // node.attr(
+              //   'transform',
+              //   `translate(${translate[0]}, ${translate[1]}) scale(${scale})`
+              // );
+            }
+            this.createChart(countries, svg);
+            
+          })
+          .each(function(d) {
+            const node = d3.select(this);
+            if (d.selected){
+              var bounds = path.bounds(d),
+              dx = bounds[1][0] - bounds[0][0],
+              dy = bounds[1][1] - bounds[0][1],
+              x = (bounds[0][0] + bounds[1][0]) / 2,
+              y = (bounds[0][1] + bounds[1][1]) / 2,
+              scale = 0.9 / Math.max(dx / width, dy / height),
+              translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+            node.attr(
+              'transform',
+              `translate(${translate[0]}, ${translate[1]}) scale(${scale})`
+            );
+
             }
           });
 
-        const zoom = d3
-          .zoom()
-          .on('zoom', (event) => {
-            svgG.attr('transform', event.transform);
-          })
-          .scaleExtent([1, 40]);
-        svg.call(zoom);
+          //! state stuff
+        // const zoom = d3
+        //   .zoom()
+        //   .on('zoom', (event) => {
+        //     svgG.attr('transform', event.transform);
+        //   })
+        //   .scaleExtent([1, 40]);
+        // svg.call(zoom);
 
-        const states = topojson.feature(world, (world as any).objects.states)
-          .features;
+        // const states = topojson.feature(world, (world as any).objects.states)
+        //   .features;
 
-        // console.log((world as any).objects)
-        svgG
-          .append('g')
-          .attr('class', 'boundary state hidden')
-          .selectAll('boundary')
-          .data(states)
-          .enter()
-          .append('path')
-          .attr('name', function (d) {
-            return d.properties.name;
-          })
-          .attr('id', function (d) {
-            return d.id;
-          })
-          // .on('click', selected)
-          // .on("mousemove", showTooltip)
-          // .on("mouseout",  function(d,i) {
-          //     tooltip.classed("hidden", true);
-          //  })
-          .attr('d', path);
-      });
+        // // console.log((world as any).objects)
+        // svgG
+        //   .append('g')
+        //   .attr('class', 'boundary state hidden')
+        //   .selectAll('boundary')
+        //   .data(states)
+        //   .enter()
+        //   .append('path')
+        //   .attr('name', function (d) {
+        //     return d.properties.name;
+        //   })
+        //   .attr('id', function (d) {
+        //     return d.id;
+        //   })
+          
+          // .attr('d', path);
+      // });
 
     // ! show states on zoom level
     // TODO:  http://bl.ocks.org/MaciejKus/61e9ff1591355b00c1c1caf31e76a668
